2c2a3db1467b99bee7a1af93fb43227c7e085b60
ruotingw <wruoting@gmail.com>
2017-06-23 09:29:11 -0400

    Html Adds

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -3,6 +3,7 @@ package controllers
 import javax.inject._
 import play.api._
 import play.api.mvc._
+import play.libs._
 
 /**
  * This controller creates an `Action` to handle HTTP requests to the
@@ -19,6 +20,10 @@ class HomeController @Inject() extends Controller {
    */
   def index = Action {
     Ok(views.html.index("Your new application is ready...")) //calling index method in views
+  }
+
+  def webGL = Action {
+    Ok(views.html.webGL())
   }
 
 
